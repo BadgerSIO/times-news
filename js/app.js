2c2eec68ee56e6b44fe6024ba78937f426acf7f0
BadgerSIO <saadibneomar@gmail.com>
2022-09-03 12:21:44 +0600

    added news cards

--- js/app.js
+++ js/app.js
@@ -47,3 +47,74 @@ function displayall(category_id, catagory_name) {
   displayNews();
 }
 loadData();
+//showing news
+const getNews = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/category/01`;
+  const res = await fetch(url);
+  const data = await res.json();
+  showNews(data.data);
+};
+
+const showNews = (data) => {
+  const newsContainer = document.getElementById("newsContainer");
+  data.forEach((news) => {
+    const singleNews = document.createElement("div");
+    singleNews.classList.add(
+      "singleNews",
+      "bg-white",
+      "p-4",
+      "rounded",
+      "flex",
+      "space-x-4",
+      "mb-5"
+    );
+    singleNews.innerHTML = `
+        <div class="thumbnail">
+              <img src="${news.thumbnail_url}" alt="" />
+            </div>
+            <div
+              class="newsinfo flex-1 space-y-5 py-2 flex flex-col justify-between"
+            >
+              <div class="news">
+                <h1 class="font-bold text-2xl news-title">
+                  ${news.title}
+                </h1>
+                <p class="text-base text-gray-500 py-5 news-description">
+                  ${news.details}
+                </p>
+              </div>
+              <div class="others grid grid-cols-3 ">
+                <div class="author_info flex items-center space-x-3">
+                  <div class="author_img">
+                    <img src="${news.author.img}" alt="" class="w-12" />
+                  </div>
+                  <div class="authorname text-sm">
+                    <h5 class="font-bold capitalize">${news.author.name}</h5>
+                    <h6>${news.author.published_date}</h6>
+                  </div>
+                </div>
+                <div class="viewcount flex items-center justify-center">
+                  <h5 class="flex justify-center items-center self-center">
+                    <span class="pr-2"
+                      ><img src="images/eye.svg" alt=""
+                    /></span>
+                    <span id="total_views">${news.total_view}</span>
+                  </h5>
+                </div>
+                <div class="flex justify-end items-center">
+                  <img
+                    src="images/seemore.svg"
+                    alt=""
+                    class="cursor-pointer"
+                    title="See details"
+                    id="showNewsDetails"
+                    onclick="showNewsDetail(${news._id})"
+                  />
+                </div>
+              </div>
+            </div>
+        `;
+    newsContainer.appendChild(singleNews);
+  });
+};
+getNews();
